c631fd83cec829a946b60bfa4fa2aa9cd36f6e43
Dmitry <dmkhost@gmail.com>
2020-11-19 21:34:14 +0200

    fixed script.js

--- js/script.js
+++ js/script.js
@@ -3,19 +3,26 @@
 const locationBtns = document.querySelectorAll('.location__btn');
 const showMoreBtn = document.querySelector('.widget__show-more');
 const widgetHidden = document.querySelectorAll('.widget__hidden');
+const valueHidden = document.querySelector('[name = "value-05"]');
 
 
 locationBtns.forEach(btn => {
-	btn.addEventListener('click', () => {
+	btn.addEventListener('click', (event) => {
+		event.preventDefault();
 		btn.classList.toggle('location__btn');
 		btn.classList.toggle('location__btn--active');
 	});
 });
 
-showMoreBtn.addEventListener('click', () => {
+showMoreBtn.addEventListener('click', (event) => {
+	event.preventDefault();
 	widgetHidden.forEach(i => {
-		// i.classList.remove('widget__hidden');
 		i.classList.toggle('widget__hidden');
+		if (widgetHidden[0].classList.contains('widget__hidden')) {
+			showMoreBtn.innerHTML = `Показать еще`;
+		} else {
+			showMoreBtn.innerHTML = `Скрыть`;
+		}
 	});
-	// showMoreBtn.innerHTML = `Скрыть`;
 });
+
