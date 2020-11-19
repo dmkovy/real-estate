8cdb7d9088fc95729c6a1a5adb53be79f3b26d1c
Dmitry <dmkhost@gmail.com>
2020-11-19 16:31:34 +0200

    changed sidebar & script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const locationBtns = document.querySelectorAll('.location__btn');
+const showMoreBtn = document.querySelector('.widget__show-more');
+const widgetHidden = document.querySelectorAll('.widget__hidden');
+
+
+locationBtns.forEach(btn => {
+	btn.addEventListener('click', () => {
+		btn.classList.toggle('location__btn');
+		btn.classList.toggle('location__btn--active');
+	});
+});
+
+showMoreBtn.addEventListener('click', () => {
+	widgetHidden.forEach(i => {
+		// i.classList.remove('widget__hidden');
+		i.classList.toggle('widget__hidden');
+	});
+	// showMoreBtn.innerHTML = `Скрыть`;
+});
